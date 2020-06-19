d2c4d11a544d76c7da4f4034677f35ac59d2144d
Kuba <kuba.alicki@gmail.com>
2020-06-19 00:58:48 -0400

    Added render current stored domains to Whitelist page (issue #15)

--- src/options/views/whitelist-view/whitelist-view.js
+++ src/options/views/whitelist-view/whitelist-view.js
@@ -9,9 +9,44 @@ const headings = {
     subtitle: "Create a custom list to send 'Do-Not-Sell' signals"
 }
 
+async function buildList() {
+    var stored_domains = []
+    var items = ""
+    chrome.storage.local.get(["DOMAINS"], function (result) {
+        stored_domains = result.DOMAINS;
+        for (var domain in result.DOMAINS) {
+            items += 
+                `
+        <li>
+            <div class="uk-width-1-1" style="font-size: medium;">
+                `
+                +
+                domain
+                +
+                `
+            <span
+                class="uk-badge uk-align-right"
+                style="
+                    margin: 0px;
+                    background-color: white;
+                    border: 1px solid #f44336;
+                    color: #f44336;
+                    "
+                >
+                    Delete
+                </span>
+            </div>
+        </li>
+                `
+        }
+        document.getElementById('whitelist-main').innerHTML = items;
+    })
+}
+
 export async function whitelistView(scaffoldTemplate) {
     const body = renderParse(scaffoldTemplate, headings, 'scaffold-component')
     let content = await fetchParse('./views/whitelist-view/whitelist-view.html', 'whitelist-view')
+    buildList();
 
     document.getElementById('content').innerHTML = body.innerHTML
     document.getElementById('scaffold-component-body').innerHTML = content.innerHTML
