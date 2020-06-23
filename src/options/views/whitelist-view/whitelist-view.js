b8f93fb245421c276737b8ea37aacb472eb207b7
Kuba <kuba.alicki@gmail.com>
2020-06-22 20:01:04 -0400

    Attempted to add listener for Whitelist page search bar, needs fix (issue #15)

--- src/options/views/whitelist-view/whitelist-view.js
+++ src/options/views/whitelist-view/whitelist-view.js
@@ -3,10 +3,63 @@ whitelist-view script
 */
 
 import { renderParse, fetchParse } from '../../components/util.js'
+// import '../../libs/list.js-1.5.0/list.min.js'
 
 const headings = {
     title: 'Whitelist',
     subtitle: "Create a custom list to send 'Do-Not-Sell' signals"
+}
+
+function eventListeners() {
+    console.log("Loaded eventListeners();")
+    document.getElementById('searchbar').addEventListener('keyup', () => {
+        console.log("Searchbar listener activated...")
+        
+        input = document.getElementById('searchbar')
+        filter = input.value.toLowerCase()
+    })
+}
+
+// Constructs list of domains with `list.min.js`
+async function buildListJS() {
+    var options = {
+        valueNames: [ 'domain' ],
+        // Since there are no elements in the list, this will be used as template.
+        item: `<li>
+        <div class="uk-width-1-1 domain" style="font-size: medium;">
+          <span
+            class="uk-badge uk-align-right"
+            style="
+              margin: 0px;
+              background-color: white;
+              border: 1px solid #f44336;
+              color: #f44336;
+            "
+          >
+            Delete</span
+          >
+        </div>
+      </li>`
+      };
+      
+    var values = [];
+
+    // Populate values
+    chrome.storage.local.get(["DOMAINS"], function (result) {
+        for (var d in result.DOMAINS) {
+            values += { domain: d }
+        }
+    })
+      
+    var domainList = new List('whitelist-view', options, values);
+      
+    // chrome.storage.local.get(["DOMAINS"], function (result) {
+    //     for (var d in result.DOMAINS) {
+    //         domainList.add({
+    //             domain: d,
+    //           });
+    //     }
+    // })
 }
 
 async function buildList() {
@@ -18,7 +71,7 @@ async function buildList() {
             items += 
                 `
         <li>
-            <div class="uk-width-1-1" style="font-size: medium;">
+            <div class="uk-width-1-1 domain" style="font-size: medium;">
                 `
                 +
                 domain
@@ -46,8 +99,13 @@ async function buildList() {
 export async function whitelistView(scaffoldTemplate) {
     const body = renderParse(scaffoldTemplate, headings, 'scaffold-component')
     let content = await fetchParse('./views/whitelist-view/whitelist-view.html', 'whitelist-view')
-    buildList();
-
+    // var values = { valueNames: ['domain'] }
+    // var domainList = new List('whitelist-view', values)
+    
     document.getElementById('content').innerHTML = body.innerHTML
     document.getElementById('scaffold-component-body').innerHTML = content.innerHTML
+    buildList();
+    console.log("Finished building list.")
+
+    eventListeners();
 }
