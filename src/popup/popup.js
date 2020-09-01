1627a7c073d0eea58f8234ae3dbcdb3b521dfa5e
asalia-1 <asalia@wesleyan.edu>
2020-09-01 15:32:59 -0400

    remove domain and dns switch on null domain (issue #67)

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -4,18 +4,16 @@ Copyright (c) 2020 Kuba Alicki, David Baraka, Rafael Goldstein, Sebastian Zimmec
 privacy-tech-lab, https://privacy-tech-lab.github.io/
 */
 
-
 /*
 popup.js
 ================================================================================
 popup.js supplements and renders complex elements on popup.html
 */
 
-
-import { 
-  // toggleListener, 
-  addToDomainlist, 
-  removeFromDomainlist 
+import {
+  // toggleListener,
+  addToDomainlist,
+  removeFromDomainlist,
 } from "../domainlist.js";
 
 /**
@@ -23,7 +21,7 @@ import {
  * @param {Object} event - contains information about the event
  */
 document.addEventListener("DOMContentLoaded", (event) => {
-  var parsed_domain = '';
+  var parsed_domain = "";
 
   /**
    * Queries, parses, and sets active tab domain to popup
@@ -32,16 +30,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
     var tab = tabs[0];
     try {
       var url = new URL(tab.url);
-      var parsed = psl.parse(url.hostname)
+      var parsed = psl.parse(url.hostname);
       parsed_domain = parsed.domain;
       console.log("POPUP: ", parsed_domain);
       if (parsed_domain === null) {
-        // Change this conditional in the future 
-        // if (location.href.substr(0,6) === "chrome") {
-          document.getElementById("domain").innerHTML = "Local webpage (null)";
-        // } else {
-          // document.getElementById("domain").innerHTML = location.href;
-        // }
+        document.getElementById("dns-body").style.display = "none";
+        document.getElementById("domain").style.display = "none";
       } else {
         document.getElementById("domain").innerHTML = parsed_domain;
       }
@@ -112,16 +106,16 @@ document.addEventListener("DOMContentLoaded", (event) => {
    */
   chrome.storage.local.get(["DOMAINS"], function (result) {
     // Sets popup view
-    var checkbox = ""
-    var text = ""
+    var checkbox = "";
+    var text = "";
     if (result.DOMAINS[parsed_domain]) {
       checkbox = `<input type="checkbox" id="input" checked/>
-                      <span></span>`
-      text = "Do Not Sell Enabled"
+                      <span></span>`;
+      text = "Do Not Sell Enabled";
     } else {
       checkbox = `<input type="checkbox" id="input"/>
-                      <span></span>`
-      text = "Do Not Sell Disabled"
+                      <span></span>`;
+      text = "Do Not Sell Disabled";
     }
     document.getElementById("switch-label").innerHTML = checkbox;
     document.getElementById("dns-text").innerHTML = text;
@@ -130,25 +124,24 @@ document.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("switch-label").addEventListener("click", () => {
       chrome.storage.local.set({ ENABLED: true, DOMAINLIST_ENABLED: true });
       chrome.storage.local.get(["DOMAINS"], function (result) {
-        var t = ""
+        var t = "";
         if (result.DOMAINS[parsed_domain]) {
-          t = "Do Not Sell Disabled"
+          t = "Do Not Sell Disabled";
           removeFromDomainlist(parsed_domain);
         } else {
-          t = "Do Not Sell Enabled"
+          t = "Do Not Sell Enabled";
           addToDomainlist(parsed_domain);
         }
         // console.log(t)
         document.getElementById("dns-text").innerHTML = t;
-      })
-    })
-  })
-
-})
+      });
+    });
+  });
+});
 
 /**
  * Builds the requested domains HTML of the popup window
- * @param {Object} requests - Contains all request domains for the current tab 
+ * @param {Object} requests - Contains all request domains for the current tab
  * (requests = tabs[activeTabID].REQUEST_DOMAINS; passed from background page)
  */
 async function buildDomains(requests) {
@@ -173,7 +166,9 @@ async function buildDomains(requests) {
         class="uk-width-1-1 uk-margin-small-top"
         style="font-size: small; font-weight: bold;"
       >
-        Responses: <span class="uk-badge" style="font-size: 10px; background-color: #cfd8dc; color:#37474f !important;">RECEIVED 0/`+Object.keys(requests[request_domain].URLS).length+`</span>
+        Responses: <span class="uk-badge" style="font-size: 10px; background-color: #cfd8dc; color:#37474f !important;">RECEIVED 0/` +
+      Object.keys(requests[request_domain].URLS).length +
+      `</span>
       </div>
       <div class="uk-width-1-1">
         None
@@ -194,8 +189,8 @@ chrome.runtime.sendMessage({
 });
 
 /**
- * Listens for messages from background page that call functions to populate 
- * the popup badge counter and build the popup domain list HTML, respectively 
+ * Listens for messages from background page that call functions to populate
+ * the popup badge counter and build the popup domain list HTML, respectively
  */
 chrome.runtime.onMessage.addListener(function (request, _, __) {
   if (request.msg === "BADGE") {
