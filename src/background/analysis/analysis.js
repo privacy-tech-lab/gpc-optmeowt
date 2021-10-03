83e7c24d06ae2f56b9fb5724767eb8f26f155edb
Oliver Wang <owang@wesleyan.edu>
2021-10-03 14:30:51 -0400

    Brought issue further up to date with main, implemented dns link finder that identifies by searching header streams (Issue #223)

--- src/background/analysis/analysis.js
+++ src/background/analysis/analysis.js
@@ -37,7 +37,11 @@ import { modes } from "../../data/modes.js";
 import { defaultSettings } from "../../data/defaultSettings.js";
 import { stores, storage } from "./../storage.js";
 // import { debug } from "webpack";
-// import psl from "psl";
+import psl from "psl";
+import { onBeforeSendHeaders } from "../protection/events.js";
+import { IS_BROWSER } from "../../theme/darkmode.js";
+import { headers } from "../../data/headers"
+import { popperOffsets } from "@popperjs/core";
 
 
 
@@ -50,7 +54,8 @@ import { stores, storage } from "./../storage.js";
 
 var analysis = {};
 var urlFlags;
-var hasReloaded = true;
+// var hasReloaded = false;
+
 // var domainlist = {};    // Caches & mirrors domainlist in storage
 // var mode = defaultSettings["MODE"]; // Caches the extension mode
 // var isDomainlisted = defaultSettings["IS_DOMAINLISTED"];
@@ -61,6 +66,12 @@ var hasReloaded = true;
 // var sendSignal = true;  // Caches if the signal can be sent to the curr domain
 
 
+var sendingGPC = false;
+var changingSitesOnAnalysis = false;
+var changingSitesOnUserRequest = false;  // used to create new analysis section
+// use this to bump all teh variables up one
+
+
 /*
 
 Analysis object prototype structure
@@ -68,7 +79,7 @@ Analysis object prototype structure
 var analysis = {
   "wired.com": {  // DOMAIN LEVEL
     0: {
-      "FIRST": {  // All of the info here will be scraped if privacy flag found
+      "BEFORE_GPC": {  // All of the info here will be scraped if privacy flag found
         "COOKIES": {
           "usprivacy": {
             domain: "www.wired.com",
@@ -99,7 +110,7 @@ var analysis = {
           // 'RECURSIVE' 2nd DOMAIN LEVEL
           "https://eus.rubiconproject.com/usync.html?us_privacy=1---": {
             0: {
-              "FIRST": {
+              "BEFORE_GPC": {
                 "COOKIES": {},
                 "HEADERS": {},
                 "URLS": {
@@ -108,14 +119,14 @@ var analysis = {
                 "USPAPI": {},
                 "USPAPI_LOCATOR": {}
               }, 
-              "SECOND": {
+              "AFTER_GPC": {
                 ...
               }
             }
           }
         }
       },
-      "SECOND": {
+      "AFTER_GPC": {
         ...
       }
     },
@@ -136,6 +147,59 @@ var analysis = {
     "CONFLICTS": 
 }
 
+
+var analysis = {
+  "wired.com": [  // DOMAIN LEVEL
+    {
+      "TIMESTAMP": {},
+      "BEFORE_GPC": {  // All of the info here will be scraped if privacy flag found
+        "COOKIES": {
+          "usprivacy": {
+            domain: "www.wired.com",
+            expirationDate: 1663019064,
+            firstPartyDomain: "",
+            hostOnly: true,
+            httpOnly: false,
+            name: "usprivacy",
+            path: "/",
+            sameSite: "lax",
+            secure: false,
+            session: false,
+            storeId: "firefox-default",
+            value: "1---"
+          }
+        },
+        "DO_NOT_SELL_LINK_EXISTS": false,
+        "HEADERS": {},
+        "URLS": {},
+        "USPAPI": {
+          "uspString": {
+            uspString: "1---",
+            version: 1
+          }
+        },
+        "USPAPI_LOCATOR": {}, // Not sure if we need this here
+        "THIRD_PARTIES": {
+          "https://eus.rubiconproject.com/usync.html?us_privacy=1---": 
+          [
+            {
+              "BEFORE_GPC": {
+                "COOKIES": {},
+                "HEADERS": {},
+                "URLS": { "us_privacy": "1---" },
+                "USPAPI": {},
+                "USPAPI_LOCATOR": {}
+              }, 
+              "AFTER_GPC": { ... }
+            }
+          ]
+        }
+      },
+      "AFTER_GPC": { ... }
+    }
+  ]
+}
+
 */
 
 
@@ -146,6 +210,10 @@ var analysis = {
 /******************************************************************************/
 
 
+/**
+ * 
+ * @returns 
+ */
 function loadFlags() {
   var urlFlags = [];
   //Load the privacy flags from the static json file
@@ -166,6 +234,12 @@ function loadFlags() {
 // 1: details 2: did we privatize this request?
 // Firefox implementation for fingerprinting classification flags
 // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/onBeforeSendHeaders
+/**
+ * 
+ * @param {*} details 
+ * @param {*} privatized 
+ * @returns 
+ */
 function processTrackingRequest (details, privatized){
   var urlFlags = loadFlags();
 
@@ -188,6 +262,11 @@ function processTrackingRequest (details, privatized){
   return details
 }
 
+/**
+ * 
+ * @param {*} url 
+ * @returns 
+ */
 function parseURLForSignal(url) {
   var flagSettingsDict = [];
 
@@ -214,68 +293,220 @@ function parseURLForSignal(url) {
 
 /******************************************************************************/
 /******************************************************************************/
-/**********          # Functions & Listeners                         **********/
+/**********                       # Functions                        **********/
 /******************************************************************************/
 /******************************************************************************/
 
+
+// Listener parameters for webRequest & webNavigation
+const MOZ_REQUEST_SPEC = ["requestHeaders", "blocking"];
+const MOZ_RESPONSE_SPEC = ["responseHeaders", "blocking"];
+const FILTER = { urls: ["<all_urls>"] };
 
 let newIncognitoTab = chrome.windows.create({ "url": null, "incognito": true });
 
-let usprivacyRegex = /us-?_?privacy/g;
+// /us-?_?privacy/g;
+let cookiesRegex = new RegExp([
+  /(us-?_?privacy)|/,
+  /(OptanonConsent)/
+].map(r => r.source).join(''), "gmi");
 
-// Listener parameters for webRequest & webNavigation
-const MOZ_REQUEST_SPEC = ["requestHeaders", "blocking"]
-const MOZ_RESPONSE_SPEC = ["responseHeaders", "blocking"]
-const FILTER = { urls: ["<all_urls>"] }
+console.log(cookiesRegex);
 
+function webRequestFiltering(details){
+  //console.log("function called");
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder();
 
-// webRequest and webNaviation listeners - for grabbing HTTP headers
-// chrome.webRequest.onBeforeSendHeaders.addListener(
-// 	onBeforeSendHeaders,
-// 	FILTER,
-// 	MOZ_REQUEST_SPEC
-// )
-// chrome.webRequest.onHeadersReceived.addListener(
-// 	onHeadersReceived,
-// 	FILTER,
-// 	MOZ_RESPONSE_SPEC
-// )
-chrome.webNavigation.onBeforeNavigate.addListener((result) => {
-  console.log("Value of hasReloaded before change: ", hasReloaded)
-  hasReloaded = !hasReloaded;
-})
+  filter.ondata = event => {
+    let str = decoder.decode(event.data, {stream: true});
+    //console.log(str);
+    let phrasing = /(Do.Not|Don.t).Sell.(My)?/gmi
 
-function runAnalysis() {
-  hasReloaded = false;
-  console.log("Immediately initializing page reload... standby from onCommited... ")
-  console.log("Finished loading page. Curr hasReloaded = ", hasReloaded);
-  if (!hasReloaded) {
-    chrome.tabs.reload();
-    hasReloaded = true;
+    if (phrasing.test(str)){
+      console.log("found it as header");
+    }
+    filter.write(encoder.encode(str));
+    filter.disconnect();
   }
 }
-// chrome.webNavigation.onCommitted.addListener((result) => {
-//   console.log("Immediately initializing page reload... standby from onCommited... ")
-//   console.log("Finished loading page. Curr hasReloaded = ", hasReloaded);
-//   if (!hasReloaded) {
-//     chrome.tabs.reload();
-//   }
-// })
 
-// window.addEventListener('load', (event) => {
-//   console.log("Finished loading page. Curr hasReloaded = ", hasReloaded);
-//   if (!hasReloaded) {
-//     chrome.tabs.reload();
-//   }
-// })
+
+function addHeaders(details) {
+  webRequestFiltering(details);
+  for (let signal in headers) {
+    let s = headers[signal]
+    details.requestHeaders.push({ name: s.name, value: s.value })
+  }
+  return { requestHeaders: details.requestHeaders }
+}
+
+
+/**
+ * Initializes the analysis with a refresh after being triggered
+ */
+function runAnalysis() {
+  // console.log("Reloading site, sendingGPC =", sendingGPC);
+  sendingGPC = true;
+  changingSitesOnAnalysis = true;
+  addGPCHeaders();
+  chrome.tabs.reload();
+}
+
+function disableAnalysis() {
+  // console.log("DISABLING ANALYSIS, REMOVING GPC HEADERS")
+  sendingGPC = false;
+  changingSitesOnAnalysis = false;
+  removeGPCHeaders();
+}
+
+/**
+ * Runs `dom.js` to attach DOM signal
+ * @param {object} details - retrieved info passed into callback
+ */
+ function addDomSignal(details) {
+  chrome.tabs.executeScript(details.tabId, {
+    file: "dom.js",
+    frameId: details.frameId, // Supposed to solve multiple injections
+                              // as opposed to allFrames: true
+    runAt: "document_start",
+  });
+}
+
+/**
+ * https://developer.chrome.com/docs/extensions/reference/history/#transition_types 
+ * @param {transitionType} transition 
+ * @returns bool
+ */
+ function isValidTransition(transition) {
+  return (transition === "link"
+    || transition === "typed"
+    || transition === "generated"
+    || transition === "reload"
+    || transition === "keyword"
+    || transition === "keyword_generated" // Potentially unneeded
+  );
+}
+
+/**
+ * Returns url domain: String
+ * @param {String} url 
+ */
+ function parseURL(url) {
+  let urlObj = new URL(url);
+  return (psl.parse(urlObj.hostname)).domain;
+}
+
+
+var analysisDataSkeletonThirdParties = () => {
+  return {
+    "TIMESTAMP": null,
+    "COOKIES": [],
+    "HEADERS": {},
+    "URLS": {},
+    "USPAPI": [],
+    "USPAPI_LOCATOR": {}
+  }
+}
+
+var analysisDataSkeletonFirstParties = () => { 
+  return {
+    "BEFORE_GPC": {
+      "TIMESTAMP": null,
+      "COOKIES": [],
+      "DO_NOT_SELL_LINK_EXISTS": null,
+      "HEADERS": {},
+      "URLS": {},
+      "USPAPI": [],
+      "USPAPI_LOCATOR": {},
+      "THIRD_PARTIES": {}
+    },
+    "AFTER_GPC": {
+      "TIMESTAMP": null,
+      "COOKIES": [],
+      "DO_NOT_SELL_LINK_EXISTS": null,
+      "HEADERS": {},
+      "URLS": {},
+      "USPAPI": [],
+      "USPAPI_LOCATOR": {},
+      "THIRD_PARTIES": {}
+    },
+    "SENT_GPC": null
+  }
+}
 
 /**
  * 
  * @param {Object} data 
  * Parameters - type: STRING, data: ANY
  */
-function logData(data) {
+function logData(domain, command, data) {
+  let gpcStatusKey = sendingGPC ? "AFTER_GPC" : "BEFORE_GPC";
+  // let gpcStatusKey = changingSitesOnUserRequest ? "BEFORE_GPC" : "AFTER_GPC";
 
+  if (!analysis[domain]) {
+    // console.log("Adding analysis[domain] = [];")
+    analysis[domain] = [];
+  }
+  let callIndex = analysis[domain].length;
+  // console.log("call index: ", callIndex)
+
+  // FIX TEH USE CASE HERE FOR ARRAYS
+
+  if (changingSitesOnUserRequest) {
+    analysis[domain][callIndex] = analysisDataSkeletonFirstParties();
+    changingSitesOnUserRequest = false;
+  } else {
+    callIndex -= 1;
+    // console.log("Saving to minus one callindex", callIndex)
+    // console.log("(4) analysis: ", analysis);
+  }
+
+  if (!analysis[domain][callIndex][gpcStatusKey]["TIMESTAMP"]) {
+    let ms = Date.now();
+    analysis[domain][callIndex][gpcStatusKey]["TIMESTAMP"] = ms; 
+  }
+
+  if (sendingGPC) {
+    analysis[domain][callIndex]["SENT_GPC"] = true;
+  }
+
+  // Let's assume that data does have a name property as a cookie should
+  if (command === "COOKIES") {
+    analysis[domain][callIndex][gpcStatusKey]["COOKIES"].push(data);
+    // console.log("Got to COMMAND === COOKIES");
+
+    // Make a new enumerated section under the particular domain
+    // otherwise use the last one
+  }
+  if (command === "USPAPI") {
+    // console.log("Got to COMMAND === USPAPI");
+    analysis[domain][callIndex][gpcStatusKey]["USPAPI"].push(data);
+  }
+  console.log("Updated analysis logs: ", analysis);
+}
+
+
+
+/******************************************************************************/
+/******************************************************************************/
+/**********                       # Listeners                        **********/
+/******************************************************************************/
+/******************************************************************************/
+
+
+var addGPCHeaders = function() {
+  sendingGPC = true;
+  chrome.webRequest.onBeforeSendHeaders.addListener(
+    addHeaders,
+    FILTER,
+    MOZ_REQUEST_SPEC
+);}
+
+var removeGPCHeaders = function() {
+  sendingGPC = false;
+  chrome.webRequest.onBeforeSendHeaders.removeListener(addHeaders);
 }
 
 // Cookie listener - grabs ALL cookies as they are changed
@@ -284,23 +515,47 @@ let listenerForUSPCookies = chrome.cookies.onChanged.addListener(
     if (!changeInfo.removed) {
       let cookie = changeInfo.cookie;
       let domain = cookie.domain;
+      domain = domain[0] == '.' ? domain.substring(1) : domain;
+      let urlObj = psl.parse(domain);
 
-      if (usprivacyRegex.test(cookie.name)) {
-        if (!analysis[domain]) analysis[domain] = {};
-        let length = Object.keys(analysis[cookie.domain]).length;
-
-        analysis[cookie.domain][length+1] = cookie;
+      if (cookiesRegex.test(cookie.name)) {
+        // console.log("Init logData() from listenerForUSPCookies")
+        // console.log("logData domain: ", urlObj.domain)
+        logData(urlObj.domain, "COOKIES", cookie);
       }
     }
-    console.log(analysis);
+    // console.log(analysis);
+})
+
+chrome.webNavigation.onCommitted.addListener((details) => {
+// https://developer.chrome.com/docs/extensions/reference/history/#transition_types
+  let validTransition = isValidTransition(details.transitionType);
+  console.log("transitionType: ", details.transitionType);
+
+  // changingSitesOnAnalysis, changingSitesOnUserRequest, sendingGPC
+  if (validTransition) {
+    if (changingSitesOnAnalysis) {
+      // add SENDING GPC TO FILE
+      // Turn off changing sites on analysis 
+      addDomSignal();
+      changingSitesOnAnalysis = false;
+    } else {  // Must be on user request
+      disableAnalysis();
+      changingSitesOnUserRequest = true;
+    }
+  }
 })
 
 // Message passing listener - for collecting USPAPI call data from window
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.msg === "USPAPI_TO_BACKGROUND") {
-    console.log("Data from USPAPI returned to background", message.data);
-    console.log("Message object: ", message);
-    logData(domain, "COOKIES", message.data);
+    let url = new URL(message.location);
+    let domain = parseURL(url);
+    // console.log("Data from USPAPI returned to background", message.data);
+    // console.log("Message object: ", message);
+    // console.log("Init logData() from runtime.onMessage")
+    // console.log("logData domain: ", domain)
+    logData(domain, "USPAPI", message.data);
   }
   if (message.msg === "RUN_ANALYSIS") {
     runAnalysis();
@@ -311,10 +566,6 @@ chrome.runtime.onConnect.addListener(function(port) {
   port.onMessage.addListener(function (message) {
     if (message.msg === "RUN_ANALYSIS_FROM_BACKGROUND") {
       runAnalysis();
-      // chrome.runtime.sendMessage({
-      //   msg: "RUN_ANALYSIS",
-      //   data: null,
-      // });
     }
   })
 })
---
+++ src/content-scripts/contentScript.js
@@ -0,0 +1,163 @@
+/*
+OptMeowt is licensed under the MIT License
+Copyright (c) 2021 Kuba Alicki, Stanley Markman, Oliver Wang, Sebastian Zimmeck
+Previous contributors: Kiryl Beliauski, Daniel Knopf, Abdallah Salia
+privacy-tech-lab, https://privacytechlab.org/
+*/
+
+
+/*
+contentScripts.js
+================================================================================
+contentScripts.js runs on every page and passes data to the background page
+https://developer.chrome.com/extensions/content_scripts
+*/
+
+
+// Here is a resource I used to help setup the inject script functionality as 
+// well as setup message listeners to pass data back to the background
+// https://www.freecodecamp.org/news/chrome-extension-message-passing-essentials/
+
+
+/******************************************************************************/
+/******************************************************************************/
+/**********              # USPAPI call helper functions              **********/
+/******************************************************************************/
+/******************************************************************************/
+
+
+// To be injected to call the USPAPI function in analysis mode
+const uspapi = `
+  try {
+    __uspapi('getUSPData', 1, (data) => {
+      console.log("USP Data: ", data);
+      let currURL = document.URL
+      window.postMessage({ type: "USPAPI_TO_CONTENT_SCRIPT", result: data, url: currURL });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+`
+
+const runAnalysisProperty = `
+if (!window.runAnalysis) {
+    window.runAnalysis = function() {
+		console.log("POSTED RUN_ANALYSIS");
+		window.postMessage({ type: "RUN_ANALYSIS", result: null });
+        return 333
+    };
+};`
+
+function injectScript(script) {
+  const scriptElem = document.createElement('script');
+  scriptElem.innerHTML = script;
+  document.documentElement.prepend(scriptElem);
+}
+
+function dnsLinkFinder() {
+  console.log("Initializing dnsLinkFinder();")
+  var tagtypes = ["a","button","footer"]; //tag types to search for
+	// var phrasing = /Do.Not.Sell.\(My\)?|Don't.Sell.\(My\)?/gmi
+	var phrasing = /(Do.Not|Don.t).Sell.(My)?/gmi
+
+	for (let x=0; x<tagtypes.length;x++){
+		var elements = document.getElementsByTagName(tagtypes[x]);
+		for (let i = 0; i<elements.length; i++){
+			var element = elements[i];
+			var text = element.innerHTML;
+			if (phrasing.test(text)){
+				console.log("Found it, here is the DNS", text);
+				break;
+			}
+		}
+	}
+}
+
+
+
+/******************************************************************************/
+/******************************************************************************/
+/**********                   # Main functionality                   **********/
+/******************************************************************************/
+/******************************************************************************/
+
+
+/**
+ * Passes info to background scripts for processing via messages
+ * https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/sendMessage
+ * There are other ways to do this, but I use an IIFE to run everything at once
+ * https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+ */
+(async () => {
+
+	/* MAIN CONTENT SCRIPT PROCESSES GO HERE */
+
+  console.log("MAIN CONTENT SCRIPT INIT:: ");
+
+	let url = new URL(location.href);
+
+	/* (1) Gets Frame:0 Tab content */
+	chrome.runtime.sendMessage({
+		msg: "CONTENT_SCRIPT_TAB",
+		data: Date.now(),
+	});
+
+	/* (2) Searches for DNS link */
+	window.onload = function() {
+    // dnsLinkFinder();
+    console.log("Initializing dnsLinkFinder();")
+    var tagtypes = ["a","button","footer"]; //tag types to search for
+    // var phrasing = /Do.Not.Sell.\(My\)?|Don't.Sell.\(My\)?/gmi
+    var phrasing = /(Do.Not|Don.t).Sell.(My)?/gmi
+
+    for (let x=0; x<tagtypes.length;x++){
+      var elements = document.getElementsByTagName(tagtypes[x]);
+      for (let i = 0; i<elements.length; i++){
+        var element = elements[i];
+        var text = element.innerHTML;
+        if (phrasing.test(text)){
+          console.log("Found it, here is the DNS", text);
+          break;
+        }
+      }
+	  }
+    injectScript(uspapi);
+		injectScript(runAnalysisProperty);
+	}
+
+	/* (3) Fetches .well-known GPC file */
+	const response = await fetch(`${url.origin}/.well-known/gpc.json`);
+	const wellknownData = await response.json();
+
+	chrome.runtime.sendMessage({
+		msg: "CONTENT_SCRIPT_WELLKNOWN",
+		data: wellknownData,
+	});
+
+})();
+
+
+
+/******************************************************************************/
+/******************************************************************************/
+/**********    # Message passing from injected script via window     **********/
+/******************************************************************************/
+/******************************************************************************/
+
+
+window.addEventListener('message', function(event) {
+// console.log("EVENT: ", event);
+  if (event.data.type == "USPAPI_TO_CONTENT_SCRIPT"
+    // && typeof chrome.app.isInstalled !== 'undefined'
+  ) {
+    console.log("USPAPI_RETURNed to contentScript.js!", event.data.result);
+    chrome.runtime.sendMessage({ 
+      msg: "USPAPI_TO_BACKGROUND", 
+      data: event.data.result, 
+      location: this.location.href 
+    });
+  }
+	if (event.data.type == "RUN_ANALYSIS") {
+		chrome.runtime.sendMessage({ msg: "RUN_ANALYSIS", data: event.data.result });	
+	}
+}, false);
