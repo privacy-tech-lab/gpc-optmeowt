a0f9fa59cfefa3d86e49faba6473f89ab4e19f7c
sophieeng <seng@wesleyan.edu>
2022-10-03 13:47:27 -0400

    testing w/ error (issue #365)

--- src/tests/background/validCookie.test.js
+++ src/tests/background/validCookie.test.js
@@ -1,6 +1,6 @@
 
-/*
-import { isValidSignalIAB } from "./cookiesIAB"; */
+
+import { isValidSignalIAB } from "./cookiesIAB"; 
 
 /* Test mocking
 jest.mock("../../background/cookiesIAB", () => jest.fn());
@@ -10,9 +10,7 @@ beforeEach(() => {
 });
 */
 
-/*
 test('IAB signal is valid', () =>{
     const IABsignal = isValidSignalIAB('1---');
     expect(IABsignal).toBeTruthy();
 });
-*/
