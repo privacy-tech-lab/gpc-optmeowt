1ce9d7ec0165fd46cf3e7e273ada729f4550e2f0
sophieeng <seng@wesleyan.edu>
2023-01-02 18:25:22 -0800

    isValidSignalIAB test (issue #392)

--- test/background/isValidSignalIAB.test.js
+++ test/background/isValidSignalIAB.test.js
@@ -1,6 +1,22 @@
 import assert from 'assert';
 import {isValidSignalIAB} from '../../src/background/cookiesIAB.js';
 
-it('should accept 1--- as true string', () => {
-    assert.equal(isValidSignalIAB("1---"), true);
+describe('Check different IAB signals for validity', () => {
+    it('should accept 1--- as true string', () => {
+        assert.equal(isValidSignalIAB("1---"), true);
+    });
+    it('should accept 1YNY as true string', () => {
+        assert.equal(isValidSignalIAB("1YNY"), true);
+    });
+    it('should reject 1-- as false string', () => {
+        assert.equal(isValidSignalIAB("1--"), false);
+    });
+    it('should reject 2YYY as false string', () => {
+        assert.equal(isValidSignalIAB("2YYY"), false);
+    });
+    it('should reject 1WWW as false string', () => {
+        assert.equal(isValidSignalIAB("1WWW"), false);
+    });
+
 });
+
