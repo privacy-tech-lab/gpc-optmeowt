1ef202fefeb9db99b6c2fb689cddb0c65fe9e43b
Oliver Wang <owang@wesleyan.edu>
2023-02-10 14:22:38 -0500

    More GPC test modifications (Issue #392)

--- test/background/gpc.test.js
+++ test/background/gpc.test.js
@@ -2,10 +2,8 @@
  *  Tests for testing GPC signals
  */
 
- import harness from '../background/helpers/harness.js';
- import backgroundWait from '../background/helpers/backgroundWait.js';
- import pageWait from '../background/helpers/pageWait.js';
  import assert from 'assert';
+ import puppeteer from 'puppeteer';
 
 
  import http from 'http';
@@ -17,48 +15,64 @@
  let teardown
  
  describe('GPC test', function () {
-   this.timeout(20000);
+   this.timeout(20000000);
      before(async () => {
-         ({ browser, bgPage, teardown } = await harness.setup())
-         await backgroundWait.forAllConfiguration(bgPage)
 
-        //  chrome.scripting.registerContentScripts([
-        //     {
-        //       id: "1",
-        //       matches: ["<all_urls>"],
-        //       js: ["content-scripts/registration/gpc-dom.js"],
-        //       runAt: "document_start",
-        //     },
-        //     {
-        //       id: "2",
-        //       matches: ["https://example.org/foo/bar.html"],
-        //       js: ["content-scripts/registration/gpc-remove.js"],
-        //       runAt: "document_start",
-        //     },
-        //   ]);
+        const puppeteerOps = {
+            headless: 'chrome',
+        }
+        const args = []
+
+        let extensionPath = 'dev/chrome'
+
+        args.push('--disable-extensions-except=' + extensionPath)
+        args.push('--load-extension=' + extensionPath)
+
+        puppeteerOps.args = args
+        browser = await puppeteer.launch(puppeteerOps)
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
 
      })
      after(async () => {
-         await teardown()
+        await new Promise(resolve => setTimeout(resolve, 1000));
+         await browser.close()
      })
  
      it('Tests whether the GPC signal is properly set', async () => {         
              const page = await browser.newPage()
-             await pageWait.forGoto(page, `https://good.third-party.site/privacy-protections/gpc/`)
-             // Wait for injection; will be resolved with MV3 changes
-             await page.waitForFunction(
-                 () => navigator.globalPrivacyControl,
-                 { polling: 100, timeout: 6000 }
-             )
-             await page.evaluate(() => {
-                 document.getElementById('start').click()
-             })
-             await new Promise(resolve => setTimeout(resolve, 1000));
-             await document.getElementById('tests').click()
-             let top_frame_header = document.getElementById('test-top-frame header').innerHTML;
-             let top_frame_api = document.getElementById('test-top-frame JS API').innerHTML;
-             assert(top_frame_header, 1);
-             assert(top_frame_api, "true");
+             let top_frame_header;
+             let top_frame_api;
+             //let gpc;
+             await page.goto(`https://example.com`)
 
+             await new Promise(resolve => setTimeout(resolve, 2000));
+             //await page.reload();
+             //await new Promise(resolve => setTimeout(resolve, 1000));
+
+             const gpc = await page.evaluate(() => {
+                    return (async () => {
+                        return navigator.globalPrivacyControl
+                    })()
+                })
+            
+            //  await page.evaluate(async () => {
+            //     gpc = await document.window.navigator.globalPrivacyControl;
+            //     // await new Promise(resolve => setTimeout(resolve, 3000));
+
+            //     // document.getElementById('start').click()
+             
+            //     // await new Promise(resolve => setTimeout(resolve, 3000));
+
+            //     // document.getElementById('tests').click()
+
+            //     // await new Promise(resolve => setTimeout(resolve, 3000));
+            //     // top_frame_header = document.getElementById('test-top-frame header').innerHTML;
+            //     // top_frame_api = document.getElementById('test-top-frame JS API').innerHTML;
+            //  })
+
+            assert.equal(gpc, true);
+            //  assert.equal(top_frame_header, 1);
+            //  assert.equal(top_frame_api, "true");
      })
  })
