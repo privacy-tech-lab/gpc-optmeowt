beb0da5bd93436eb52c6f18c448e96762a437976
Oliver Wang <owang@wesleyan.edu>
2023-02-15 14:52:45 -0500

    Added negative GPC test (Issue #392)

--- test/background/gpc.test.js
+++ test/background/gpc.test.js
@@ -37,13 +37,12 @@
              const page = await browser.newPage()
              let rules;
              //let gpc;
-             await page.goto(`https://global-privacy-control.glitch.me/`)
 
-             //await new Promise(resolve => setTimeout(resolve, 2000));
-             await page.reload();
-             //await new Promise(resolve => setTimeout(resolve, 1000));
 
-             const gpc = await page.evaluate(async () => {
+            await page.goto("https://example.org/foo/bar.html")
+            await page.reload();
+
+            const gpc_neg = await page.evaluate(async () => {
                 await new Promise(resolve => setTimeout(resolve, 2000));
 
                     return (async () => {
@@ -51,19 +50,36 @@
                     })()
                 })
 
-            const header = await page.evaluate(async () => {
+                await page.goto(`https://global-privacy-control.glitch.me/`)
 
-                function getElementByXpath(path) {
-                    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-                  }
+                //await new Promise(resolve => setTimeout(resolve, 2000));
+                await page.reload();
+                //await new Promise(resolve => setTimeout(resolve, 1000));
+   
+                const gpc = await page.evaluate(async () => {
+                   await new Promise(resolve => setTimeout(resolve, 2000));
+   
+                       return (async () => {
+                           return navigator.globalPrivacyControl
+                       })()
+                   })
+   
+               const header = await page.evaluate(async () => {
+   
+                   function getElementByXpath(path) {
+                       return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+                     }
+   
+                   return getElementByXpath("/html/body/section[2]/div/div[1]/div/h3").innerText;
+                   
+               })
 
-                return getElementByXpath("/html/body/section[2]/div/div[1]/div/h3").innerText;
-                
-            })
+
             
 
             
             assert.equal(header, 'Header present \nSec-GPC: "1"');
             assert.equal(gpc, true);
+            assert.equal(gpc_neg, undefined);
      })
  })
