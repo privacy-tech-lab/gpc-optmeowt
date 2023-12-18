7bb8a93828901e20da88b860faee7686ccf96733
Oliver Wang <owang@wesleyan.edu>
2023-12-18 12:12:46 -0500

    Changed CI test (Issue #443)

--- test/background/parseIAB.test.js
+++ test/background/parseIAB.test.js
@@ -15,17 +15,23 @@ import { parseIAB } from "../../src/background/cookiesIAB.js";
 
 describe("Check parsing of IAB signal", () => {
     it('Should parse invalid signal to 1NYN', () => {
-        assert.equal(parseIAB("1WYY"), '1NYN');
+        assert.equal(parseIAB("1WYY", true), '1NYN');
     });
 
     it('Should parse valid signal 1--- to 1YYY', () => {
-        assert.equal(parseIAB("1---"), '1YYY');
+        assert.equal(parseIAB("1---",true ), '1YYY');
     });
 
     it('Should change third char in valid signal to Y', () => {
-        assert.equal(parseIAB("1NNN"), '1NYN');
-        assert.equal(parseIAB("1YNY"), '1YYY');
-        assert.equal(parseIAB("1NNY"), '1NYY');
+        assert.equal(parseIAB("1NNN", true), '1NYN');
+        assert.equal(parseIAB("1YNY", true), '1YYY');
+        assert.equal(parseIAB("1NNY", true), '1NYY');
+    });
+
+    it('Should change third char in valid signal to N', () => {
+        assert.equal(parseIAB("1NYN", true), '1NNN');
+        assert.equal(parseIAB("1YYN", true), '1YNN');
+        assert.equal(parseIAB("1YYY", true), '1YNY');
     });
     
 }) 
